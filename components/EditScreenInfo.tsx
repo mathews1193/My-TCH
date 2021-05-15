3c16f0769c708f2ba7ac9d5f3c3510fd37bb889a
mathews1193 <mathews1193@yahoo.com>
2021-05-14 22:53:07 -0500

    first commit

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -10,19 +10,6 @@ export default function EditScreenInfo({ path }: { path: string }) {
   return (
     <View>
       <View style={styles.getStartedContainer}>
-        <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Open up the code for this screen:
-        </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
-          darkColor="rgba(255,255,255,0.05)"
-          lightColor="rgba(0,0,0,0.05)">
-          <MonoText>{path}</MonoText>
-        </View>
 
         <Text
           style={styles.getStartedText}
